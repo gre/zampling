c0c190a2a67ec04fe07a7ce46099fdaf21d1718b
Gaëtan Renaudeau <gre@zenexity.com>
2013-10-02 12:14:05 +0200

    implement createFromArrayBuffer

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@
 
   var ctx = new (window.webkitAudioContext || window.AudioContext)();
 
-  var track = QaudioXHR(ctx, "musics/circus.mp3").then(Zampling.Track.createFromArrayBuffer);
-
-  track.then(function(t){
-    T = t;
+  var track = QaudioXHR(ctx, "musics/circus.mp3").then(function (audioBuffer) {
+    return Zampling.Track.createFromArrayBuffer(audioBuffer.getChannelData(0), ctx);
   });
 
   $("input[type='file']").change(function() {
     QaudioFileInput(ctx, this).then(function(buf) {
       Zampling.Track.createFromArrayBuffer(buf)
     })
-  })
+  });
+
+  track.then(function(t){
+    T = t;
+  }).done();
 }());
--- src/models/Track.js
+++ src/models/Track.js
@@ -11,11 +11,30 @@ Zampling.Track = Backbone.Model.extend({
     throw "Not Implemented";
   }
 }, {
-  createFromArrayBuffer: function (arrayBuffer) {
+  DEFAULT_SAMPLES_SIZE: 44100,
+  createFromArrayBuffer: function (float32ArrayBuffer, ctx, samplesSize) {
+    if (!float32ArrayBuffer || float32ArrayBuffer.length === 0) throw "float32ArrayBuffer is empty.";
     // TODO cut in multiple chunks
-    var chunks = new Zampling.ChunkNode(new Zampling.Chunk(arrayBuffer), null);
+    if (!samplesSize) samplesSize = Zampling.Track.DEFAULT_SAMPLES_SIZE;
+
+    var length = float32ArrayBuffer.length;
+    
+    var chunks = [];
+    for (var i=0; i<length; i += samplesSize) {
+      var size = Math.min(length-i, samplesSize);
+      var audioBuffer = ctx.createBuffer(1, size, ctx.sampleRate);
+      var floatArray = audioBuffer.getChannelData(0);
+      floatArray.set(float32ArrayBuffer.subarray(i, i+size));
+      var chunk = new Zampling.Chunk(floatArray, audioBuffer);
+      chunks.push(chunk);
+    }
+
+    var head = _.reduceRight(chunks, function (nextChunk, chunk) {
+      return new Zampling.ChunkNode(chunk, nextChunk);
+    }, null);
+    
     return new Zampling.Track({
-      chunks: chunks
+      chunks: head
     });
   }
 });
