3665fffbad063f84306a09d7a4eaa6b557c79b1a
ybr <ybr@zenexity.com>
2013-10-02 12:23:35 +0200

    Player

--- src/main.js
+++ src/main.js
@@ -8,9 +8,23 @@
     T = t;
   });
 
+  var buffer = null
+
   $("input[type='file']").change(function() {
     QaudioFileInput(ctx, this).then(function(buf) {
+      buffer = buf
       Zampling.Track.createFromArrayBuffer(buf)
+    }).then(function() {
+      $("div#controls").show()
     })
+  })
+
+  var player = new Zampling.Player()
+
+  $("button#play").click(function() {
+    player.play(buffer, player.offset())
+  })
+  $("button#stop").click(function() {
+    player.stop()
   })
 }());
--- src/models/Player.js
+++ src/models/Player.js
@@ -1,3 +1,24 @@
 Zampling.Player = Backbone.Model.extend({
+  initialize: function() {
+    this.ctx = new (window.webkitAudioContext || window.AudioContext)();
+    var gain = this.ctx.createGain()
+    gain.gain.value = 0.5
 
+    this.destination = gain
+    this.destination.connect(this.ctx.destination)
+  },
+  play: function(buffer, offset) {
+    var source = this.ctx.createBufferSource()
+    source.buffer = buffer
+    source.connect(this.destination)
+    source.start(0, offset)
+    this.source = source
+  },
+  stop: function() {
+    this.source.stop(0)
+    return this.ctx.currentTime
+  },
+  offset: function() {
+    return this.ctx.currentTime || 0
+  }
 });
