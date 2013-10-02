ec83ea3e675d52d75b7b605d803265a137b60c76
Valerian Barbot <vbr@zenexity.com>
2013-10-02 15:00:04 +0200

    auto download

--- src/main.js
+++ src/main.js
@@ -38,7 +38,11 @@
     hf.download = new Date().toISOString() + '.wav';
     hf.innerHTML = hf.download;
 
-    $(hf).appendTo("#wrapper")
+    $(hf).appendTo("#wrapper");
+    
+    var click = document.createEvent("Event");
+    click.initEvent("click", true, true);
+    hf.dispatchEvent(click);
   })
 
   track.then(function(t){
