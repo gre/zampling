10b7ebe4bd2dc1493f92661f15a2adea40c2aafd
Nizar Sehli <nse@zenexity.com>
2013-10-02 15:58:38 +0200

    Splicing chunks and chunkNodes

--- src/models/Chunk.js
+++ src/models/Chunk.js
@@ -3,8 +3,19 @@ Zampling.Chunk = function(samples, audioBuffer) {
   this.audioBuffer = audioBuffer;
 }
 
-Zampling.Chunk.prototype.splice = function(start, end) {
+Zampling.Chunk.prototype.splice = function(at, ctx) {
+  var audioBuffer1 = ctx.createBuffer(1, at, ctx.sampleRate),
+      audioBuffer2 = ctx.createBuffer(1, (this.samples.length - at), ctx.sampleRate),
+      floatArray1 = audioBuffer1.getChannelData(0),
+      floatArray2 = audioBuffer2.getChannelData(0);
 
+  floatArray1.set(this.samples.subarray(0, at));
+  floatArray2.set(this.samples.subarray(at, this.samples.length));
+
+  var chunk1 = new Zampling.Chunk(floatArray1, audioBuffer1),
+      chunk2 = new Zampling.Chunk(floatArray2, audioBuffer2);
+
+  return [chunk1, chunk2];
 }
 
 Zampling.ChunkNode = function(chunk, nextChunkNode)  {
@@ -37,4 +48,12 @@ Zampling.ChunkNode.prototype.reverse = function() {
     reversed.next = clone
   }
   return reversed
+}
+
+Zampling.ChunkNode.prototype.splice = function(at, ctx) {
+  var chunks = this.chunk.splice(at, ctx),
+      chunkNode2 = new Zampling.ChunkNode(chunks[1], this.next),
+      chunkNode1 = new Zampling.ChunkNode(chunks[0], chunkNode2);
+
+  return [chunkNode1, chunkNode2];
 }
